780a4dc9867befd2b0181e103a92589e74e0b59d
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2024-11-03 21:53:07 +0200

    Setterrs and getters added

--- nba/model/NBAPlayer.java
+++ nba/model/NBAPlayer.java
@@ -21,5 +21,23 @@ public class NBAPlayer extends Player{
     public void setTeam(NBATeam team) {
         this.team = team;
     }
+    public int getPoints() {
+        return points;
+    }
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    public int getRebounds() {
+        return rebounds;
+    }
+    public void setRebounds(int rebounds) {
+        this.rebounds = rebounds;
+    }
+    public int getAssists() {
+        return assists;
+    }
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
     
 }
