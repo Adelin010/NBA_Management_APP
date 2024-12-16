f4c5752c50d01cab88924366d7b96188e21ded28
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2024-12-16 18:44:51 +0200

    Database

--- src/main/resources/sql/NBA_DB.sql
+++ src/main/resources/sql/NBA_DB.sql
@@ -35,6 +35,7 @@ create table Player(
 create table Manager(
     id int identity(1, 1),
     name varchar(255),
+    age int,
     team_id int,
     constraint Primary_Key_Manager primary key(id),
     constraint Foreign_Key_Team foreign key(team_id) references Team(id)
@@ -61,12 +62,16 @@ create table Game(
 create table Sponsor(
     id int identity(1, 1),
     name varchar(255) not null,
+    age int,
+    email varchar(255),
+    pf int,
     constraint Primary_Key_Sponsor primary key(id)
 );
 
 create table Season(
     id int identity(1, 1),
     name varchar(255) not null,
+    year int,
     constraint Primary_Key_Season primary key(id)
 );
 
@@ -83,21 +88,27 @@ create table Found(
 
 insert into Conference values ('East'), ('West')
 
-insert into Team values ('Team1', 1), ('Team2', 1), ('Team3', 2), ('Team4', 1), ('Team5', 2)
+insert into Team values ('Lakers', 1), ('Chicago Bulls', 2), ('Golden State Warrios', 1), ('Boston Celtics', 2)
 
-insert into Manager values ('Manager1', 1), ('Manager2', null), ('Manager3', 5), ('Manager4', 2), ('Manager5', 3), ('Manager6', 4)
+insert into Manager values ('Michael Gates', 45,1), ('Adelin Cojocaru', 38,2), ('Andrei Blaj', 50,3), ('Mario Lopez', 42,4)
 
-insert into Season values ('Winter2024')
+insert into Season values ('NBA2023',2023),('NBA2024',2024)
 
-insert into Player 
-values('Player1', 24, 'PG', 12030.00, 32, 4, 3, 1), ('Player2', 34, 'SG', 21000.00, 54, 13, 2, 5), 
-('Player3', 27, 'C', 32450.00, 43, 6, 5, 3), ('Player4', 24, 'SF', 43570.00, 54, 6, 23, 4), ('Player5', 24, 'PF', 51370.00, 45, 12, 23, 4),
-('Player6', 32, 'C', 43880.00, 12, 3, 12, 2), ('Player7', 25, 'C', 11232.00, 11, 1, 22, 2), ('Player8', 25, 'PF', 76794.00, 54, 6, 23, 1)
-
+insert into Player
+values
+    ('LeBron James', 39, 'PF', 1000000, 25000, 5000, 2000, 1),
+    ('Stephen Curry', 36, 'PG', 900000, 21000, 4500, 3000, 2),
+    ('Kawhi Leonard', 32, 'SF', 800000, 18000, 4000, 2500, 3),
+    ('Giannis Antetokounmpo', 29, 'PF', 1100000, 30000, 7000, 3500, 4);
 insert into Game(team1_id, team2_id, score_team1, score_team2, game_type, season_id) 
 values(1, 3, 121, 119, 'RS', 1), (2, 3, 121, 119, 'RS', 1), (1, 5, 123, 120, 'RS', 1)
 
-insert into Sponsor values('Sponsor1'), ('Sponsor2')
+insert into Sponsor
+values
+    ('Converse', NULL, 'converse@gmail.com', 0),
+    ('Cola', 45, 'cola@gmail.com', 1),
+    ('Nike', 35, 'nike@gmail.com', 1),
+    ('Adidas', 50, 'adidas@gmail.com', 1);
 
 insert into Found values(1, 2, 3000.00), (5, 2, 4000.00), (4, 1, 12000.00);
 
