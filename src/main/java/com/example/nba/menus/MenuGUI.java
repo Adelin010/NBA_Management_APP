17f1856d79b3086d117dfc084bdc58ca2b617ef1
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2025-01-07 22:25:41 +0200

    Controller changes

--- src/main/java/com/example/nba/menus/MenuGUI.java
+++ src/main/java/com/example/nba/menus/MenuGUI.java
@@ -11,7 +11,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class MenuGUI {
+public class MenuGUI extends Application{
     private final Controller controller;
     private Stage primaryStage;
     private StackPane contentArea;
